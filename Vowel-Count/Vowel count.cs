bee59a4fbb403c06fe4f9ffdfa508b1646c4a785
sbozich <92573419+sbozich@users.noreply.github.com>
2021-11-15 14:28:27 +0100

    Add files via upload

---
+++ Vowel-Count/Vowel count.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+        for (int i=0; i<str.Length;i++)
+        {
+            if (str[i]=='a'||str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+            vowelCount++;
+        }
+                 
+        Console.WriteLine(vowelCount);
+
+        return vowelCount;
+    }
+}
